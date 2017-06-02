b45f47fe1ad42ed5e5039a06ca9096c1715c4540
alan <phwpanhengwang@163.com>
2017-06-02 18:05:44 +0800

    demo

---
+++ app/src/main/java/alan/example/com/expandablelistviewdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package alan.example.com.expandablelistviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String[] groupStrings = {"西游记", "水浒传", "三国演义", "红楼梦"};
+    public String[][] childStrings = {
+            {"唐三藏", "孙悟空", "猪八戒", "沙和尚"},
+            {"宋江", "林冲", "李逵", "鲁智深"},
+            {"曹操", "刘备", "孙权", "诸葛亮", "周瑜"},
+            {"贾宝玉", "林黛玉", "薛宝钗", "王熙凤"}
+    };
+    private ExpandableListView elView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        elView = (ExpandableListView) findViewById(R.id.el_view);
+        elView.setAdapter(new MyExpandableListAdapter(this));
+        //        设置分组项的点击监听事件
+        elView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), groupStrings[i], Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+//        设置子选项点击监听事件
+        elView.setOnChildClickListener(new ExpandableListView.OnChildClickListener()
+
+        {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+                                        int childPosition, long id) {
+                Toast.makeText(getApplicationContext(), childStrings[groupPosition][childPosition], Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/alan/example/com/expandablelistviewdemo/MyExpandableListAdapter.java
@@ -0,0 +1,155 @@
+package alan.example.com.expandablelistviewdemo;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by qk14 on 2017/6/2.
+ */
+
+public class MyExpandableListAdapter implements android.widget.ExpandableListAdapter {
+
+    private Context mContext;
+
+
+    public String[] groupStrings = {"西游记", "水浒传", "三国演义", "红楼梦"};
+    public String[][] childStrings = {
+            {"唐三藏", "孙悟空", "猪八戒", "沙和尚"},
+            {"宋江", "林冲", "李逵", "鲁智深"},
+            {"曹操", "刘备", "孙权", "诸葛亮", "周瑜"},
+            {"贾宝玉", "林黛玉", "薛宝钗", "王熙凤"}
+    };
+
+    public MyExpandableListAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public int getGroupCount() {
+        return groupStrings.length;
+    }
+
+    //        获取指定分组中的子选项的个数
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return childStrings[groupPosition].length;
+    }
+
+    //        获取指定的分组数据
+    @Override
+    public Object getGroup(int groupPosition) {
+        return groupStrings[groupPosition];
+    }
+
+    //        获取指定分组中的指定子选项数据
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return childStrings[groupPosition][childPosition];
+    }
+
+    //        获取指定分组的ID, 这个ID必须是唯一的
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    //        获取子选项的ID, 这个ID必须是唯一的
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    //        分组和子选项是否持有稳定的ID, 就是说底层数据的改变会不会影响到它们。
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+    //        获取显示指定分组的视图
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        GroupViewHolder groupViewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_expand_group, parent, false);
+            groupViewHolder = new GroupViewHolder();
+            groupViewHolder.tvTitle = (TextView) convertView.findViewById(R.id.label_expand_group);
+            convertView.setTag(groupViewHolder);
+        } else {
+            groupViewHolder = (GroupViewHolder) convertView.getTag();
+        }
+        groupViewHolder.tvTitle.setText(groupStrings[groupPosition]);
+        return convertView;
+    }
+
+    //        获取显示指定分组中的指定子选项的视图
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ChildViewHolder childViewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_expand_child, parent, false);
+            childViewHolder = new ChildViewHolder();
+            childViewHolder.tvTitle = (TextView) convertView.findViewById(R.id.label_expand_child);
+            convertView.setTag(childViewHolder);
+        } else {
+            childViewHolder = (ChildViewHolder) convertView.getTag();
+        }
+        childViewHolder.tvTitle.setText(childStrings[groupPosition][childPosition]);
+        return convertView;
+    }
+
+    //        指定位置上的子元素是否可选中
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void onGroupExpanded(int i) {
+
+    }
+
+    @Override
+    public void onGroupCollapsed(int i) {
+
+    }
+
+    @Override
+    public long getCombinedChildId(long l, long l1) {
+        return 0;
+    }
+
+    @Override
+    public long getCombinedGroupId(long l) {
+        return 0;
+    }
+
+    static class GroupViewHolder {
+        TextView tvTitle;
+    }
+    static class ChildViewHolder {
+        TextView tvTitle;
+    }
+}
